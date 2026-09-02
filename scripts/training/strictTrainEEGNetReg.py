e6e0c9237aa1906eaf3911245de06b44daf5b01f
KineticJetIce245 <kineticjetice245@gmail.com>
2026-09-02 19:56:27 -0400

    usable prototype

--- scripts/training/strictTrainEEGNetReg.py
+++ scripts/training/strictTrainEEGNetReg.py
@@ -1,13 +1,13 @@
-from enum import unique
 from pathlib import Path
 
 import numpy as np
 import torch
+from torch.utils.data import DataLoader, TensorDataset
 
 from nova2026.architecture.cnn import EEGNet
 from nova2026.architecture.lossfun import FocalLoss
 from nova2026.config import DATA_DIR
-from nova2026.trainer import Trainer
+from nova2026.trainer import SupervisedTrainer
 
 ROOT = DATA_DIR / "COG-BCI"
 DATASET = ROOT / "outputs/PVT_128Hz_AttUPipeline.pt"
@@ -21,7 +21,14 @@ SEED = 0
 
 
 # model hyperparameters
+# criterion = torch.nn.CrossEntropyLoss()
 criterion = FocalLoss(gamma=3.0, alpha=[1, 3.5], reduction="mean")
+permutates = {
+    "epoch": lambda d: print(
+        f"At epoch {d['epoch']}, train_loss={d['train_loss']:.4f}, val_loss={d['best_val_loss']:.4f}"
+    ),
+    "test_output": None,
+}
 
 
 def regularizer(model, max_value: float = 1.0, eps: float = 1e-8) -> None:
@@ -41,44 +48,83 @@ def label_trials(data_chkpt: dict) -> np.ndarray:
     return np.array([0 if t < 500 else 1 for t in data_chkpt["rt"]])
 
 
-def organizer(data_chkpt: dict, test_sub: str):
-    chkpt = {
-        "data": {},
-        "labels": {},
-    }
+def organizer(
+    data_chkpt: dict,
+    test_sub: str,
+    subs: list,
+    val_frac: float = VAL_FRAC,
+    seed: int = SEED,
+):
+    print(f"Omitting subject {test_sub} from training data.")
+    chkpt = {}
     labels = label_trials(data_chkpt)
-    train_mask: np.ndarray = np.array(
-        [tags[0] != test_sub for tags in data_chkpt["metadata"]]
-    )
     data = np.asarray(data_chkpt["data"])
-    chkpt["data"]["train"] = data[train_mask]
-    chkpt["data"]["test"] = data[~train_mask]
-    chkpt["labels"]["train"] = labels[train_mask]
-    chkpt["labels"]["test"] = labels[~train_mask]
+    subs_meta = np.asarray([tags[0] for tags in data_chkpt["metadata"]])
+
+    # test DataLoader
+    test_mask = subs_meta == test_sub
+    chkpt["test"] = DataLoader(
+        TensorDataset(
+            torch.tensor(data[test_mask], dtype=torch.float32),
+            torch.tensor(labels[test_mask], dtype=torch.long),
+        ),
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+    )
+
+    # select val subjects
+    if val_frac >= 1:
+        raise ValueError("Val fraction must be less than 1.")
+    n_vals = max(1, int((len(subs) - 1) * val_frac))
+    rng = np.random.default_rng(seed)
+    subs_not_test = [s for s in subs if s != test_sub]
+    val_subs = rng.choice(subs_not_test, size=n_vals, replace=False)
+    train_subs = np.setdiff1d(subs_not_test, val_subs)
+
+    train_mask = np.isin(subs_meta, train_subs)
+    val_mask = np.isin(subs_meta, val_subs)
+
+    # train DataLoader
+    chkpt["train"] = DataLoader(
+        TensorDataset(
+            torch.tensor(data[train_mask], dtype=torch.float32),
+            torch.tensor(labels[train_mask], dtype=torch.long),
+        ),
+        batch_size=BATCH_SIZE,
+        shuffle=True,
+    )
+
+    # val DataLoader
+    chkpt["val"] = DataLoader(
+        TensorDataset(
+            torch.tensor(data[val_mask], dtype=torch.float32),
+            torch.tensor(labels[val_mask], dtype=torch.long),
+        ),
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+    )
+
     return chkpt
 
 
-def main():
-    trainer = Trainer()
-    trainer.fetch(Path(DATASET))
+trainer = SupervisedTrainer()
+trainer.fetch(Path(DATASET))
 
-    meta = trainer.chkpt["metadata"]
-    subs = list({tags[0] for tags in meta})
-    for sub in subs:  # main LOSO loop
-        # Prepare Model
-        model = EEGNet(chn=62)
-        optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+meta = trainer.chkpt["metadata"]
+subs = list({tags[0] for tags in meta})
+subs.sort(key=lambda s: int(s[-2:]))  # sort by the last two char as number
+for i, sub in enumerate(subs):  # main LOSO loop
+    # Prepare Model
+    model = EEGNet(chn=62)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
 
-        # load model and hyperparameters
-        trainer.uses(model, optimizer, criterion, regularizer)
-        trainer.organize(lambda x, s=sub: organizer(x, s))
-        d = trainer.data
-        print(
-            f"\n=== Fold test_sub={sub} | "
-            f"train {len(d['data']['train'])} | test {len(d['data']['test'])} ==="
-        )
-        trainer.train(BATCH_SIZE, EPOCHS)
-
-
-if __name__ == "__main__":
-    main()
+    # load model and hyperparameters
+    trainer.uses(model, optimizer, criterion, regularizer)
+    trainer.organize(lambda x, sb=sub, sbs=subs, sd=i: organizer(x, sb, sbs, seed=sd))
+    d = trainer.data
+    print(
+        f"=== Fold test_sub={sub} | "
+        f"train {len(d['train'].dataset)} | test {len(d['test'].dataset)} | val {len(d['val'].dataset)} ==="
+    )
+    trainer.train(EPOCHS, permutates)
+    print(f"Completed training for subject {sub}.")
--- src/nova2026/trainer.py
+++ src/nova2026/trainer.py
@@ -1,3 +1,4 @@
+import copy
 from collections.abc import Callable
 from pathlib import Path
 
@@ -11,7 +12,7 @@ def load_chkpt(dataset: Path) -> dict:
     return torch.load(dataset, weights_only=False)
 
 
-class Trainer:
+class SupervisedTrainer:
     def __init__(self):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -29,9 +30,6 @@ class Trainer:
         criterion: nn.Module,
         regularizer: Callable,
     ) -> None:
-        # optimizer = torch.optim.Adam(self.model.parameters(), lr=LR)
-        # criterion = FocalLoss(gamma=3.0, alpha=[1, 3.5], reduction="mean")
-        # criterion = torch.nn.CrossEntropyLoss()
         if model is None:
             raise ValueError("Model must be provided")
         self.model = model.to(self.device)
@@ -55,64 +53,125 @@ class Trainer:
         if self.data is None:
             raise ValueError("Data must be provided")
 
-    def _train_once(self, train_loader):
+    def _train_once(self, loader: DataLoader) -> float:
         self._ensure()
         self.model.train()
-        train_loss = 0.0
-        for batch_data, batch_target in train_loader:
+        total, count = 0.0, 0
+        # training
+        for batch_data, batch_target in loader:
             batch_data, batch_target = (
                 batch_data.to(self.device),
                 batch_target.to(self.device),
             )
 
+            # backprop
             self.optimizer.zero_grad()
             outputs = self.model(batch_data)
             loss = self.criterion(outputs, batch_target)
             loss.backward()
             self.optimizer.step()
 
+            # regularization
             if self.regularizer != None:
                 with torch.no_grad():
                     self.regularizer(self.model)
 
-            train_loss += loss.item() * batch_data.size(0)
-        return train_loss
+            total += loss.item() * batch_data.size(0)
+            count += batch_data.size(0)
+        return total / count
 
-    def _test_once(self, all_preds, all_targets, test_loader):
+    def _predict(self, loader: DataLoader, permutate: Callable):
         self._ensure()
         self.model.eval()
-        val_loss = 0.0
+        raw, targets = [], []
+        # run predictions
         with torch.no_grad():
-            for batch_data, batch_target in test_loader:
+            for batch_data, batch_target in loader:
+                batch_data = batch_data.to(self.device)
+                outputs = self.model(batch_data)
+                if permutate is not None:
+                    outputs = permutate(outputs)
+                raw.extend(outputs.cpu().numpy())
+                targets.extend(batch_target.cpu().numpy())
+        return raw, targets
+
+    def _evaluate_loss(self, loader: DataLoader) -> float:
+        self._ensure()
+        self.model.eval()
+        total, count = 0.0, 0
+        # run tests
+        with torch.no_grad():
+            for batch_data, batch_target in loader:
                 batch_data, batch_target = (
                     batch_data.to(self.device),
                     batch_target.to(self.device),
                 )
-                outputs = self.model(batch_data)
-                loss = self.criterion(outputs, batch_target)
-                val_loss += loss.item() * batch_data.size(0)
+                loss = self.criterion(self.model(batch_data), batch_target)
+                total += loss.item() * batch_data.size(0)
+                count += batch_data.size(0)
+        return total / count
 
-                preds = torch.argmax(outputs, dim=1).cpu().numpy()
-                all_preds.extend(preds)
-                all_targets.extend(batch_target.cpu().numpy())
-        return val_loss
-
-    def train(self, batch_size: int, epochs: int):
+    def train(
+        self,
+        epochs: int,
+        permutates: dict[str, Callable],
+    ) -> dict:
         try:
-            train_loader = DataLoader(
-                TensorDataset(self.data["data"]["train"], self.data["labels"]["train"]),
-                batch_size=batch_size,
-                shuffle=True,
+            train_loader = self.data["train"]
+            val_loader = self.data["val"]
+            test_loader = self.data["test"]
+        except KeyError as e:
+            raise ValueError(
+                f"KeyError{e}: Data not properly organized, call organize() first."
             )
-            test_loader = DataLoader(
-                TensorDataset(self.data["data"]["test"], self.data["labels"]["test"]),
-                batch_size=batch_size,
-                shuffle=False,
-            )
-        except KeyError:
-            raise ValueError("Data must be organized before training.")
+
+        history = []
+        best_val_loss, best_epoch, best_state = float("inf"), -1, None
 
         for epoch in range(epochs):
-            all_preds, all_targets = [], []
-            self._train_once(train_loader)
-            self._test_once(all_preds, all_targets, test_loader)
+            # train once
+            train_loss = self._train_once(train_loader)
+            log = {"epoch": epoch, "train_loss": train_loss}
+
+            # perform evaluation if needed
+            if val_loader is not None:
+                val_loss = self._evaluate_loss(val_loader)
+                log["val_loss"] = val_loss
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    best_state = copy.deepcopy(self.model.state_dict())
+                    best_epoch = epoch
+
+            history.append(log)
+
+            # perform callbacks for each epoch
+            if permutates is None:
+                raise ValueError("permutates must be provided")
+            per_epoch_consumer = permutates.get("epoch", lambda x: None)
+            per_epoch_consumer(
+                {
+                    "epoch": epoch,
+                    "train_loss": train_loss,
+                    "best_so_far": best_val_loss,
+                    "best_epoch": best_epoch,
+                    "best_state": best_state,
+                }
+            )
+
+        test_preds, test_targets = None, None
+        state_to_use = best_state if best_state is not None else self.model.state_dict()
+        self.model.load_state_dict(state_to_use)
+        if test_loader is not None:
+            test_preds, test_targets = self._predict(
+                test_loader, permutates.get("test_output", lambda x: x)
+            )
+
+        return {
+            "history": history,
+            "best_val_loss": best_val_loss,
+            "best_epoch": best_epoch,
+            "test_preds": test_preds,
+            "test_targets": test_targets,
+            "best_state": best_state,
+            "model": self.model,
+        }
